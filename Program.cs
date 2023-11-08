6eedd9be70dcf5a1edda0ecd152cb0850420d188
my portfolio <134843704+mggkgs@users.noreply.github.com>
2023-11-08 12:09:06 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See ationhttps://aka.ms/new-console-template for more inform
+Console.WriteLine("Hello, World!");
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");    
+    }
+  }
+}
